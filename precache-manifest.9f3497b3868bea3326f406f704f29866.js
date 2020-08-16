40161653ee80adbbf439ad0e24187ddacf53b8bc
mohamed <mohammed.nasr393@gmail.com>
2020-08-16 23:53:03 +0200

    Updates

---
+++ precache-manifest.9f3497b3868bea3326f406f704f29866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece6e884c82e9ebf8cab89430cd8828c",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "3448f015543dec0b5254",
+    "url": "/recipes/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "511304f4d5379ef7b541",
+    "url": "/recipes/static/js/2.d89f998f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/recipes/static/js/2.d89f998f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3448f015543dec0b5254",
+    "url": "/recipes/static/js/main.415079c5.chunk.js"
+  },
+  {
+    "revision": "469907f660187a80d306",
+    "url": "/recipes/static/js/runtime-main.d56bb698.js"
+  }
+]);
